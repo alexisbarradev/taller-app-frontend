3c0ab7b5cc2645ac9e5174a5af67108c7cb04a07
Alexis <alexis_barra_dev@outlook.com>
2025-06-21 21:38:58 -0400

    frontend actualizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { authGuard } from './services/auth.guard';
-import { RegisterComponent } from './register/register.component';
 import { ForoComponent } from './foro/foro.component';
 import { CompleteRegistrationComponent } from './complete-registration/complete-registration.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: 'complete-registration', component: CompleteRegistrationComponent },
   { path: 'foro', component: ForoComponent, canActivate: [authGuard] },
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
